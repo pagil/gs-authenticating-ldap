0305bf5407ce917e45a6b6564a8317d1edf78b99
truemate <truemate@test.com>
2015-03-15 21:10:36 +1100

    - Added implementation of the web security configuration for spring. - Use credentials:   username: joe   password: joespassword
    https://spring.io/guides/gs/authenticating-ldap/

---
+++ src/main/java/hello/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package hello;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/css/**").permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+            .formLogin();
+        
+    }
+    
+    @Configuration
+    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.ldapAuthentication()
+                    .userDnPatterns("uid={0},ou=otherpeople")
+                    .groupSearchBase("ou=groups")
+                    .contextSource().ldif("classpath:test-server.ldif");
+        }
+        
+    }
+}
